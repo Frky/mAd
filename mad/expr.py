18b8b9a850ac3e0f9f7bde8049bcd33694b6fa18
Franck De Goër <degoerdf@ensimag.fr>
2016-08-03 11:04:30 +0200

    First step for Markdown arithmetic: addition and multiplication

---
+++ mad/expr.py
@@ -0,0 +1,36 @@
+#-*- coding: utf-8 -*-
+
+import re
+from operator import add, mul
+
+from token import Token
+from rule import Rule
+
+class Expr(object):
+
+    calc_map = {
+                    'ADD': add,
+                    'MULT': mul,
+            }
+
+    def __init__(self, expr):
+        self.__expr = expr
+        self.__rules = list()
+        self.__tokens = list()
+        self.__parse()
+
+    def __parse(self):
+        split_expr = re.findall('[\d.]+|[%s]' % ''.join(Token.token_map), self.__expr)
+        self.__tokens = [Token(x) for x in split_expr]
+
+    def eval(self):
+        # TODO For now, no priority of MULT over ADD
+        # TODO handle errors (eg if int parsing fails)
+        res = int(self.__tokens.pop(0).value)
+        while len(self.__tokens) != 0:
+            # Get operation
+            op = self.__tokens.pop(0).name
+            # Get second operand
+            right = int(self.__tokens.pop(0).value)
+            res = Expr.calc_map[op](res, right)
+        return res
---
+++ mad/format.md
@@ -0,0 +1,13 @@
+$( arithmetics ) -> compute the value inside $( )
+
+$(( arithmetics )) -> compute the value and print the result
+
+
+Examples:
+
+* $(2 + 3) -> compute 5 
+* $(( 2 + 3 )) -> compute 5 and print it into the output file
+
+* $(a = 50) -> affect variable a w/ value 50
+* $(( a = 50 )) -> affect variable a w/ value 50 and print its value into the output file
+
