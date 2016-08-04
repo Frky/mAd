d688059d6112282d4bd899a56881a974174bf539
Franck De Goër <degoerdf@ensimag.fr>
2016-08-04 15:52:56 +0200

    Add some function descriptions and remove dead code

--- mad/src/ast/expr.py
+++ mad/src/ast/expr.py
@@ -5,22 +5,6 @@ import re
 from src.ast.token import Token
 from src.ast.opstack import OpStack
 
-class Tree(object):
-
-    def __init__(self, tok, left=None, right=None):
-        self.__token = tok
-        self.__left = left
-        self.__right = right
-        # Compute node value
-        if tok.type == Token.NUM:
-            self.__value = tok.value
-        elif tok.is_binary():
-            self.__value = tok.brain()(left.value, right.value)
-
-    @property
-    def value(self):
-        return self.__value
-
 class Expr(object):
 
     def __init__(self, expr):
@@ -30,22 +14,43 @@ class Expr(object):
         self.__parse_tokens()
 
     def __parse_tokens(self):
+        """
+            From input string, parse tokens
+
+        """
         split_expr = re.findall('[\d.]+|[%s]' % ''.join(Token.token_map), self.__expr)
         self.__tokens = [Token(x) for x in split_expr]
         self.__tokens.append(Token('EOF'))
 
     def __next(self):
+        """
+            Next token to be treated in expression
+
+        """
         # TODO handle EOF
         return self.__tokens[0]
 
     def __consume(self):
+        """
+            Return the next token to be treated and remove it from
+            the list of tokens
+
+        """
         # TODO handle EOF
         return self.__tokens.pop(0)
     
     def __error(self, msg):
+        """
+            Raise a syntax error
+
+        """
         raise Exception(msg)
 
     def __expect(self, tok):
+        """
+            Is the token what we expected at this point?
+
+        """
         if self.__next() == tok:
             return self.__consume()
         else:
@@ -66,6 +71,11 @@ class Expr(object):
             self.__stack.pop_operator()
 
     def parse(self):
+        """
+            Parse syntaxically the succession of tokens
+            And evaluate the expression
+
+        """
         self.__e()
         self.__expect(Token('EOF'))
         return self.__stack.top_operand()
@@ -73,3 +83,4 @@ class Expr(object):
     def eval(self):
         # TODO handle errors (eg if int parsing fails)
         return self.parse()
+
